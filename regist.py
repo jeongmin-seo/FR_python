18dabca95fc267d024b14873e240e51f3ddc307f
jeongmin-seo <ya4615@naver.com>
2025-08-06 14:52:46 +0900

    first commit

---
+++ regist.py
@@ -0,0 +1,142 @@
+import sys
+import cv2
+import insightface
+import face_align
+
+from PyQt5.QtWidgets import (
+    QApplication, QLabel, QWidget, QVBoxLayout, QHBoxLayout,
+    QLineEdit, QPushButton, QFileDialog
+)
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer
+from pymilvus import MilvusClient
+
+
+
+class VideoApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        # --- DB ---
+        self.client = MilvusClient("./milvus_demo.db")
+
+        # --- 얼굴인식 모델 ---
+        self.det = insightface.model_zoo.retinaface.RetinaFace("./buffalo_l/det_10g.onnx")
+        self.rec = insightface.model_zoo.arcface_onnx.ArcFaceONNX("./buffalo_l/w600k_r50.onnx")
+
+        self.kps = None
+        self.box = None
+        self.align_img = None
+        self.is_webcam = True
+        
+        self.setWindowTitle("PyQt5 Webcam Viewer + Number Input")
+
+        # --- UI 구성 ---
+        self.image_label = QLabel()
+        self.input_id_field = QLineEdit()
+        self.input_id_field.setPlaceholderText("ID를 입력하세요")
+        self.input_name_field = QLineEdit()
+        self.input_name_field.setPlaceholderText("이름을 입력하세요")
+        self.submit_button = QPushButton("저장")
+        self.submit_button.clicked.connect(self.on_submit)
+        self.upload_button = QPushButton("이미지 업로드")
+        self.upload_button.clicked.connect(self.upload_image)
+
+        # 레이아웃 구성
+        input_layout = QHBoxLayout()
+        input_layout.addWidget(self.input_id_field)
+        input_layout.addWidget(self.input_name_field)
+        input_layout.addWidget(self.submit_button)
+        input_layout.addWidget(self.upload_button)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.image_label)
+        main_layout.addLayout(input_layout)
+        self.setLayout(main_layout)
+
+        # --- 비디오 스트림 ---
+        self.cap = cv2.VideoCapture(1)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(30)
+
+    def update_frame(self):
+
+        if not self.is_webcam:
+            return
+
+        ret, frame = self.cap.read()
+        if ret:
+            self.box = None
+            self.kps = None
+            self.align_img = None
+
+            self.face_detect(frame)
+
+
+    def upload_image(self):
+        self.is_webcam = False
+
+        file_path, _  = QFileDialog.getOpenFileName(self, "이미지 선택", "", "Image Files (*.png *.jpg *.jpeg)")
+
+        if file_path:
+            image = cv2.imread(file_path)
+            if image is None:
+                print("이미지 불러얼 수 없습니다.")
+                return  # raise Error 로 수정
+
+        self.face_detect(image)
+
+    def face_detect(self, image):
+
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        rgb_image = cv2.resize(rgb_image, (640, 640))
+
+        boxes, kpss = self.det.detect(rgb_image, input_size=(640, 640))
+
+        if boxes.size:
+            r = boxes[0]
+            self.box = boxes[0]
+            self.kps = kpss[0]
+            cv2.rectangle(rgb_image, (int(r[0]), int(r[1])), (int(r[2]), int(r[3])), (0, 255, 0), 2)
+            self.align_img = face_align.norm_crop(rgb_image, landmark=kpss[0], image_size=112)
+
+            h, w, ch = rgb_image.shape
+            img = QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888)
+            self.image_label.setPixmap(QPixmap.fromImage(img))
+        else:
+            print("FAILED DETECT FACE")
+
+
+    def on_submit(self):
+        face_id = self.input_id_field.text()
+        user_name = self.input_name_field.text()
+        try:
+            face_id = int(face_id)
+        
+        except ValueError:
+            print("유효한 숫자를 입력하세요.")
+
+        if self.box is None:
+                print("BOX 검출이 안됐습니다.")
+            
+        else:
+            print("BOX 검출")
+            face_id = int(face_id)
+            print(f"입력된 숫자: {face_id}, 입력된 이름:{user_name}")
+
+            vector = self.rec.get_feat(self.align_img).flatten()
+            
+            data = [
+                {"id": face_id, "vector": vector, "name": user_name}
+            ]
+
+            print(data)
+
+            res = self.client.insert(collection_name="demo_collection", data=data)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = VideoApp()
+    window.show()
+    sys.exit(app.exec_())
---
+++ server.py
@@ -0,0 +1,225 @@
+# webcam을 mediaRTX서버로 보내서 서버에서 추론하게 만드는 RTSP를 사용한 추론 코드
+import asyncio
+import websockets
+import threading
+import queue
+import gi
+import cv2
+import numpy as np
+import insightface
+import face_align
+import time
+import json
+import time
+
+from pymilvus import MilvusClient
+
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GLib
+
+Gst.init(None)
+
+connected_clients = {}  # cam_id: websocket
+frame_queues = {}       # cam_id: queue.Queue()
+main_event_loop = None  # asyncio 메인 루프
+
+class RTSPStream:
+    def __init__(self, rtsp_url, cam_id):
+        self.rtsp_url = rtsp_url
+        self.cam_id = cam_id
+        self.running = True
+        self.pipeline = None
+        self.loop = None
+
+    def build_pipeline(self):
+        self.pipeline = Gst.parse_launch(
+            f'rtspsrc location={self.rtsp_url} latency=100 ! decodebin ! videoconvert ! video/x-raw, format=RGB ! appsink name=sink'
+        )
+        self.appsink = self.pipeline.get_by_name('sink')
+        self.appsink.set_property('emit-signals', True)
+        self.appsink.connect('new-sample', self.on_new_sample)
+
+        self.loop = GLib.MainLoop()
+        self.bus = self.pipeline.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect("message", self.on_bus_message)
+
+    def on_new_sample(self, sink):
+        sample = sink.emit('pull-sample')
+        if sample is None:
+            return Gst.FlowReturn.ERROR
+
+        buf = sample.get_buffer()
+        caps = sample.get_caps()
+        width = caps.get_structure(0).get_value('width')
+        height = caps.get_structure(0).get_value('height')
+
+        success, map_info = buf.map(Gst.MapFlags.READ)
+        if not success:
+            return Gst.FlowReturn.ERROR
+
+        frame = np.frombuffer(map_info.data, dtype=np.uint8).reshape((height, width, 3))
+        buf.unmap(map_info)
+
+        if self.cam_id in frame_queues and not frame_queues[self.cam_id].full():
+            timestamp_ms = int(time.time() * 1000)
+            frame_queues[self.cam_id].put([frame, timestamp_ms])
+
+        return Gst.FlowReturn.OK
+
+    def on_bus_message(self, bus, message):
+        t = message.type
+        if t == Gst.MessageType.ERROR:
+            err, debug = message.parse_error()
+            print(f"[{self.cam_id}] GStreamer Error: {err}")
+            self.pipeline.set_state(Gst.State.NULL)
+            self.loop.quit()
+
+    def start(self):
+        while True:
+            try:
+                print(f"[{self.cam_id}] RTSP 스트림 연결 시도 중... {self.rtsp_url}")
+                self.build_pipeline()
+                self.pipeline.set_state(Gst.State.PLAYING)
+                self.loop.run()
+                print(f"[{self.cam_id}] 스트리밍 루프 종료됨")
+            except Exception as e:
+                print(f"[{self.cam_id}] RTSP 연결 예외 발생: {e}")
+            print(f"[{self.cam_id}] 3초 후 재시도...")
+            time.sleep(3)
+
+    def stop(self):
+        if self.pipeline:
+            self.pipeline.set_state(Gst.State.NULL)
+        if self.loop:
+            self.loop.quit()
+        print(f"[{self.cam_id}] RTSP 수신 종료")
+
+
+
+def inference_worker(cam_id):
+    print(f"[{cam_id}] 추론 쓰레드 시작")
+
+    det = insightface.model_zoo.retinaface.RetinaFace("./buffalo_l/det_10g.onnx")
+    rec = insightface.model_zoo.arcface_onnx.ArcFaceONNX("./buffalo_l/w600k_r50.onnx")
+    det.session.set_providers(["CUDAExecutionProvider"])
+    rec.session.set_providers(["CUDAExecutionProvider"])
+
+    client = MilvusClient("./milvus_demo.db")
+    client.load_collection(collection_name="demo_collection")
+
+    while True:
+        try:
+            frame, frame_id = frame_queues[cam_id].get(timeout=5)
+        except queue.Empty:
+            continue
+
+        print(f"[{cam_id}] 추론 프레임 처리 중")
+
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = frame[75:-75, :]
+        frame = cv2.resize(frame, (480, 640))
+
+        height, width, _ = frame.shape
+        frame = cv2.resize(frame, (640, 640))
+        boxes, kpss = det.detect(frame, input_size=(640, 640))
+        
+        scale_x = width / 640
+        scale_y = height / 640
+
+        scale = np.array([scale_x, scale_y, scale_x, scale_y])
+
+        bat = []
+        for box, kps in zip(boxes, kpss):
+            align_img = face_align.norm_crop(frame, landmark=kps, image_size=112)
+            bat.append(align_img)
+
+        result = {}
+        if bat:
+            embedding = rec.get_feat(bat).flatten().reshape((-1, 512))
+            res = client.search(
+                collection_name="demo_collection",
+                data=embedding,
+                search_params={"metric_type": "COSINE"},
+                limit=1,
+                output_fields=["id", "name"]
+            )
+
+            # frame = cv2.resize(frame, (480,640))
+            for i, box in enumerate(boxes):
+                person = res[i][0]
+                distance = person['distance']
+
+                if distance < 0.2:
+                    continue
+
+                name = person['entity']['name']
+                person_id = str(person['entity']['id'])
+
+                rescale_box = (box[:4] * scale).astype(np.int32)
+
+                # cv2.rectangle(frame, (int(rescale_box[0]), int(rescale_box[1])), (int(rescale_box[2]), int(rescale_box[3])), (0, 255, 0), 2)
+                # cv2.imwrite("0.jpg", frame)
+                
+                result[person_id] = {
+                    "name": name,
+                    "distance": distance,
+                    "box": rescale_box.tolist(),
+                    "frame_id": frame_id
+                }
+
+            print(f"[{cam_id}] 추론 결과: {result}")
+
+        if cam_id in connected_clients:
+            future = asyncio.run_coroutine_threadsafe(
+                send_result(connected_clients[cam_id], result),
+                main_event_loop
+            )
+            try:
+                future.result(timeout=3)  # 오류 발생 시 감지 가능
+            except Exception as e:
+                print(f"[{cam_id}] WebSocket 전송 오류: {e}")
+                connected_clients.pop(cam_id, None)
+
+
+async def send_result(ws, result):
+    try:
+        await ws.send(json.dumps(result))
+    except Exception as e:
+        print(f"[WebSocket] 전송 실패: {e}")
+
+
+async def websocket_handler(websocket):
+    cam_id = await websocket.recv()
+    print(f"[WebSocket] {cam_id} 연결됨")
+
+    connected_clients[cam_id] = websocket
+    frame_queues[cam_id] = queue.Queue(maxsize=10)
+
+    rtsp_url = cam_id  # cam_id에 실제 URL이 들어왔다고 가정
+
+    # RTSP 및 추론 쓰레드 시작
+    threading.Thread(target=RTSPStream(rtsp_url, cam_id).start, daemon=True).start()
+    threading.Thread(target=inference_worker, args=(cam_id,), daemon=True).start()
+
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except websockets.ConnectionClosed:
+        print(f"[WebSocket] {cam_id} 연결 종료")
+    finally:
+        connected_clients.pop(cam_id, None)
+        frame_queues.pop(cam_id, None)
+
+
+async def main():
+    global main_event_loop
+    main_event_loop = asyncio.get_running_loop()
+    async with websockets.serve(websocket_handler, "0.0.0.0", 8765):
+        print("WebSocket 서버 실행 중...")
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
